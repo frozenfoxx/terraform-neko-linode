1891c2806238e1e593bda6d496edbca208821619
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-03-22 01:37:14 -0700

    Updated and added scripts

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "linode_instance" "main" {
+  label = var.name
+  image = var.image
+  region = var.region
+  type = var.type
+  authorized_keys = var.authorized_keys
+  root_pass = var.root_pass
+
+  provisioner "file" {
+    source = "${path.module}/scripts"
+    destination = "/tmp"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 755 /tmp/*.sh",
+      "sudo /tmp/install_docker.sh"
+    ]
+  }
+}
