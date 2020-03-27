f3ab93846b41b2eb2d6c0928061723b2c77f7de9
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-03-26 18:43:35 -0700

    Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,9 @@ output "vm_name" {
 }
 
 output "vm_ip" {
-  value = linode_instance.main.ip_address
+  value = "${data.linode_networking_ip.v4.address}/${data.linode_networking_ip.v4.prefix} (${data.linode_networking_ip.v4.rdns})"
+}
+
+output "vm_ip6" {
+  value = "${data.linode_networking_ip.v6.address}/${data.linode_networking_ip.v6.prefix} (${data.linode_networking_ip.v6.rdns})"
 }
