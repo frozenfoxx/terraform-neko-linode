1f97f4255775dc9f11bd0fbb5b130e288e6640db
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-04-03 17:47:24 -0700

    Fixed typo

--- scripts/install_neko.sh
+++ scripts/install_neko.sh
@@ -26,7 +26,7 @@ run_container()
 {
   echo "Running the container..."
   docker run -it \
-    --rm
+    --rm \
     --restart=${RESTART} \
     --shm-size=${SHM_SIZE} \
     -e DISPLAY=${DISPLAY} \
